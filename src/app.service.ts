774d3745003975083c601df79ecdacab63a81886
Vladimir <boonoovovan@yandex.ru>
2020-10-30 18:20:48 +1000

    add file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! FUCK YEAH!';
+    return 'Hello World! FUCK YEAH11!';
   }
 }
